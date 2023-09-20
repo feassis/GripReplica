d4588b74ef2c1032ceafa3635ace70e97f9f5c0e
felipe assis <felipe.assis.2002@gmail.com>
2023-09-20 16:28:50 -0300

    initial commit

---
+++ Source/GRIPReplica/GRIPReplica.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GRIPReplica.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GRIPReplica, "GRIPReplica" );
---
+++ Source/GRIPReplica/GRIPReplicaGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GRIPReplicaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRIPREPLICA_API AGRIPReplicaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
